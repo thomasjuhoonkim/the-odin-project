edc4f49fada67c6039d7af5253b5bf22a424cff6
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-06-18 18:01:44 -0400

    Javascript progress

--- foundations/javascript/javascript.js
+++ foundations/javascript/javascript.js
@@ -1,2 +1,15 @@
-let a = 10;
-console.log(a);
+user = prompt("Who's there?");
+if (user === "Admin") {
+  password = prompt("Password?");
+  if (password === "TheMaster") {
+    console.log("Welcome!");
+  } else if (password === "\n" || password === "") {
+    console.log("Cancelled");
+  } else {
+    console.log("Wrong password");
+  }
+} else if (user === "\n") {
+  console.log("Cancelled");
+} else {
+  console.log("I don't know you");
+}
