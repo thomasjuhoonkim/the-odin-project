307e43e1ffa40aad36e4ebf3ba02204e10c0a118
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-06-22 17:03:31 -0400

    Add more practive javascript functions

--- foundations/javascript/javascript.js
+++ foundations/javascript/javascript.js
@@ -1,15 +1,46 @@
-user = prompt("Who's there?");
-if (user === "Admin") {
-  password = prompt("Password?");
-  if (password === "TheMaster") {
-    console.log("Welcome!");
-  } else if (password === "\n" || password === "") {
-    console.log("Cancelled");
-  } else {
-    console.log("Wrong password");
+function min(a, b) {
+  return a <= b ? a : b;
+}
+
+function pow(x, n) {
+  if (x === undefined || isNaN(x) || n === undefined || isNaN(n)) {
+    return undefined;
   }
-} else if (user === "\n") {
-  console.log("Cancelled");
-} else {
-  console.log("I don't know you");
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   () => {
+//     alert("You agreed.");
+//   },
+//   () => {
+//     alert("You cancelled the execution.");
+//   }
+// );
+
+function add7(a) {
+  return a + 7;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function capitalize(string) {
+  return string[0].toUpperCase() + string.substring(1);
+}
+
+function lastLetter(string) {
+  string = String(string);
+  return string.substring(string.length - 1);
 }
